b4ea9464aa6ecf8dd3e13fc3674a731184bbc80d
Konstantin <kalinin.konst.official@gmail.com>
2025-03-22 20:52:54 +0300

    master Доработали метод fly_to_aruco_center, чтобы дрон взлетал если не нашёл нужный маркер для центровки. Прибрались в коде модуля

--- zxc_tello/zxc.py
+++ zxc_tello/zxc.py
@@ -3,14 +3,12 @@ from djitellopy import Tello
 from zxc_tello import aruco_utils
 import cv2
 
-tello = Tello()
-tello.connect()
-
-MAX_DIAGONAL_SPEED = 40
+MAX_DIAGONAL_SPEED = 60
 MAX_STRAIGHT_SPEED = 60
 MIN_DISTANCE = 25
 FRAME_WIDTH = 240
 FRAME_HEIGHT = 320
+MAX_HEIGHT = 200
 
 
 # Get aruco telemetry data
@@ -28,12 +26,11 @@ def check_motors(drone: Tello):
 
 
 # Go to aruco marker with aruco_id
-# TODO: нормально обработать аварийную посадку при ненахождении маркера
 def fly_to_aruco_center(drone: Tello, aruco_id: int):
     stop_tello_motion = False
     while True:
-        if stop_tello_motion is True:
-            tello.send_rc_control(0, 0, 0, 0)
+        if stop_tello_motion:
+            drone.send_rc_control(0, 0, 0, 0)
             break
         frame_img = drone.get_frame_read()
         if frame_img is None:
@@ -44,19 +41,33 @@ def fly_to_aruco_center(drone: Tello, aruco_id: int):
         index = [i for i, d in enumerate(marker_details) if d[1] == aruco_id]
         if len(marker_details) > 0 and index:
             center_x, center_y = marker_details[index[0]][0]
-            img, x_distance, y_distance, distance = aruco_utils.detect_distance_from_image_center(img, center_x,
-                                                                                                  center_y)
+            img, x_distance, y_distance, distance = aruco_utils.detect_distance_from_image_center(
+                img,
+                center_x,
+                center_y
+            )
             l_r_speed = int((MAX_DIAGONAL_SPEED * y_distance) / (FRAME_WIDTH // 2) * (-1))
             f_b_speed = int((MAX_DIAGONAL_SPEED * x_distance / (FRAME_HEIGHT // 2)) * (-1))
+
+            l_r_speed = max(min(l_r_speed, MAX_DIAGONAL_SPEED), -MAX_DIAGONAL_SPEED)
+            f_b_speed = max(min(f_b_speed, MAX_DIAGONAL_SPEED), -MAX_DIAGONAL_SPEED)
             try:
                 if abs(distance) <= MIN_DISTANCE:
-                    tello.send_rc_control(0, 0, 0, 0)
+                    drone.send_rc_control(0, 0, 0, 0)
                     stop_tello_motion = True
                 else:
-                    tello.send_rc_control(l_r_speed, f_b_speed, 0, 0)
+                    drone.send_rc_control(l_r_speed, f_b_speed, 0, 0)
             except Exception as exc:
                 print("send_rc_control exception: ", exc)
-
+                drone.send_rc_control(0, 0, 0, 0)
+        else:
+            if not index:
+                if drone.get_height() < MAX_HEIGHT:
+                    drone.send_rc_control(0, 0, 15, 0)
+                else:
+                    print("markers not found")
+                    drone.send_rc_control(0, 0, 0, 0)
+                    break
         cv2.imshow('drone', img)
         key = cv2.waitKey(1) & 0xff
         if key == 27:
@@ -73,7 +84,7 @@ def get_aruco_stream(drone: Tello):
     drone.streamon()
     while True:
         if stop_tello_motion is True:
-            tello.send_rc_control(0, 0, 0, 0)
+            drone.send_rc_control(0, 0, 0, 0)
             break
         frame_img = drone.get_frame_read()
         if frame_img is None:
